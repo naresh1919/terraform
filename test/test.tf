dc76841f18fe9c9576006986cf9ee0a171440f4d
naresh1919 <49805554+naresh1919@users.noreply.github.com>
2019-10-25 12:47:11 +0530

    Update test.tf

--- test/test.tf
+++ test/test.tf
@@ -4,12 +4,18 @@ provider "aws" {
 resource "aws_vpc" "main" {
         cidr_block = "${var.vpc_cid}"
         instance_tenancy = "default"
+        tags = {
+                Name = "naresh-vpc"
+        }
 }
 resource "aws_subnet" "sub" {
         count = "${length(var.azs)}"
         vpc_id = "${aws_vpc.main.id}"
         cidr_block = "${element(var.subnet_cidr,count.index)}"
         map_public_ip_on_launch = true
+        tags = {
+                Name = "my-subnet count.index"
+        }
 }
 resource "aws_internet_gateway" "my_igw" {
         vpc_id = "${aws_vpc.main.id}"
@@ -27,3 +33,20 @@ resource "aws_route_table_association" "routs_ass" {
         subnet_id = "${aws_subnet.sub[count.index].id}"
         route_table_id = "${aws_route_table.route_table.id}"
 }
+# resource "aws_default_network_acl" "default" {
+#       default_network_acl_id = "${aws_vpc.main.default_network_acl_id}"
+# }
+resource "aws_default_route_table" "r" {
+        default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+        tags = {
+                Name = "default table"
+        }
+}
+resource "aws_subnet" "sublk" {
+        vpc_id = "${aws_vpc.main.id}"
+        cidr_block = "120.10.4.0/26"
+        map_public_ip_on_launch = true
+}
+resource "aws_route_table_association" "gate-foon" {
+        subnet_id = "${aws_subnet.sublk.id}"
+        route_table_id = "${aws_default_route_table.r.id}"
